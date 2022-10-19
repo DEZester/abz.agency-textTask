7661974ea5d8173f0693d56b4fe93a25771b9e5c
Vlad Chubar <johndoe@example.com>
2022-10-19 11:55:52 +0300

    Changes into body/header width, and create start of LandingPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from './components/Header/Header';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <LandingPage />
     </div>
   );
 }
